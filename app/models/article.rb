f54bb45ec67d492579d262419637f6b9eecb6000
Richelle Lobo <lobo.r0018@gmail.com>
2021-12-02 20:55:25 +0800

    Adds validation for title uniqueness

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
 	 validates :title, :author, :content, presence: true
+	 validates :title, uniqueness: {scope: 'title', message: 'Title Should be unique'}
 	 has_many :comments
 end
